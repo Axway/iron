73c6c278a87fe6a70cada0cb5831935cfa3bb76f
xfournet <xfournet@users.noreply.github.com>
2018-03-27 00:55:10 +0200

    Fix #32 Domain annotations should be marked as @Documented

--- iron-api/src/main/java/io/axway/iron/description/Id.java
+++ iron-api/src/main/java/io/axway/iron/description/Id.java
@@ -1,5 +1,6 @@
 package io.axway.iron.description;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,5 +12,6 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
+@Documented
 public @interface Id {
 }
