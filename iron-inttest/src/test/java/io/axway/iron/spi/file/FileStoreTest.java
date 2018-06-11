9aa8f55e448f165a4ce8a84eb46a6658b7473c52
gvauvert <gvauvert@axway.com>
2018-06-11 11:41:17 +0200

    53 Add test of store directory deletion

--- iron-inttest/src/test/java/io/axway/iron/spi/file/FileStoreTest.java
+++ iron-inttest/src/test/java/io/axway/iron/spi/file/FileStoreTest.java
@@ -1,10 +1,24 @@
 package io.axway.iron.spi.file;
 
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.function.*;
 import org.testng.annotations.Test;
+import io.axway.iron.Store;
+import io.axway.iron.StoreManager;
+import io.axway.iron.core.StoreManagerBuilder;
+import io.axway.iron.sample.command.ChangeCompanyAddress;
+import io.axway.iron.sample.command.CreateCompany;
+import io.axway.iron.sample.command.CreatePerson;
+import io.axway.iron.sample.command.DeleteCompany;
+import io.axway.iron.sample.command.PersonJoinCompany;
+import io.axway.iron.sample.command.PersonLeaveCompany;
+import io.axway.iron.sample.command.PersonRaiseSalary;
+import io.axway.iron.sample.model.Company;
+import io.axway.iron.sample.model.Person;
 import io.axway.iron.spi.SpiTestHelper;
 import io.axway.iron.spi.serializer.SnapshotSerializer;
 import io.axway.iron.spi.serializer.TransactionSerializer;
@@ -13,6 +27,10 @@ import io.axway.iron.spi.storage.TransactionStore;
 
 import static io.axway.iron.spi.file.FileTestHelper.*;
 import static io.axway.iron.spi.jackson.JacksonTestHelper.*;
+import static java.lang.Boolean.TRUE;
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileStoreTest {
 
@@ -29,6 +47,39 @@ public class FileStoreTest {
 
         SpiTestHelper.checkThatCreateCompanySequenceIsRight(transactionStoreFactory, transactionSerializer, snapshotStoreFactory, snapshotSerializer,
                                                             randomStoreName);
+    }
+
+    @Test
+    public void should() throws IOException {
+        Path filePath = Paths.get("tmp-iron-test", "iron-spi-file-inttest");
+
+        Supplier<TransactionStore> transactionStoreFactory = () -> buildFileTransactionStore(filePath, "shouldCreateCompanySequenceBeRight");
+        Supplier<SnapshotStore> snapshotStoreFactory = () -> buildFileSnapshotStore(filePath, "shouldCreateCompanySequenceBeRight");
+
+        TransactionSerializer transactionSerializer = buildJacksonTransactionSerializer();
+        SnapshotSerializer snapshotSerializer = buildJacksonSnapshotSerializer();
+
+        StoreManagerBuilder factoryBuilder = StoreManagerBuilder.newStoreManagerBuilder() //
+                .withTransactionSerializer(transactionSerializer) //
+                .withTransactionStore(transactionStoreFactory.get()) //
+                .withSnapshotSerializer(snapshotSerializer) //
+                .withSnapshotStore(snapshotStoreFactory.get()) //
+                .withEntityClass(Company.class) //
+                .withEntityClass(Person.class) //
+                .withCommandClass(ChangeCompanyAddress.class) //
+                .withCommandClass(CreateCompany.class) //
+                .withCommandClass(CreatePerson.class) //
+                .withCommandClass(DeleteCompany.class) //
+                .withCommandClass(PersonJoinCompany.class) //
+                .withCommandClass(PersonLeaveCompany.class) //
+                .withCommandClass(PersonRaiseSalary.class);
+
+        try (StoreManager storeManager = factoryBuilder.build()) {
+            Store store = storeManager.getStore("test");
+            storeManager.snapshot();
+        }
+
+        assertThat(Files.walk(filePath).sorted(reverseOrder()).map(Path::toFile).map(File::delete).collect(toSet())).containsOnly(TRUE);
     }
 }
 
