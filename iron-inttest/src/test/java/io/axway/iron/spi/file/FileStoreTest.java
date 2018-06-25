e7476d57ccbf4ff60df99a095e5b584c80c5ead5
gvauvert <gvauvert@axway.com>
2018-06-25 15:47:59 +0200

    59 - Fix snapshot not done if a first snapshot has been done without any transaction

--- iron-inttest/src/test/java/io/axway/iron/spi/file/FileStoreTest.java
+++ iron-inttest/src/test/java/io/axway/iron/spi/file/FileStoreTest.java
@@ -3,10 +3,12 @@ package io.axway.iron.spi.file;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
+import java.util.concurrent.*;
 import java.util.function.*;
 import org.testng.annotations.Test;
+import io.axway.iron.ReadOnlyTransaction;
+import io.axway.iron.Store;
 import io.axway.iron.StoreManager;
 import io.axway.iron.core.StoreManagerBuilder;
 import io.axway.iron.sample.command.ChangeCompanyAddress;
@@ -26,7 +28,11 @@ import io.axway.iron.spi.storage.TransactionStore;
 
 import static io.axway.iron.spi.file.FileTestHelper.*;
 import static io.axway.iron.spi.jackson.JacksonTestHelper.*;
+import static java.nio.file.Paths.get;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileStoreTest {
@@ -34,7 +40,7 @@ public class FileStoreTest {
     @Test
     public void shouldCreateCompanySequenceBeRight() throws Exception {
         String randomStoreName = "iron-store-" + UUID.randomUUID();
-        Path filePath = Paths.get("tmp-iron-test", "iron-spi-file-inttest");
+        Path filePath = get("tmp-iron-test", "iron-spi-file-inttest");
 
         Supplier<TransactionStore> transactionStoreFactory = () -> buildFileTransactionStore(filePath, "shouldCreateCompanySequenceBeRight");
         Supplier<SnapshotStore> snapshotStoreFactory = () -> buildFileSnapshotStore(filePath, "shouldCreateCompanySequenceBeRight");
@@ -48,7 +54,7 @@ public class FileStoreTest {
 
     @Test
     public void shouldBeAbleToDeleteFilesAfterStoreManagerClose() throws IOException, InterruptedException {
-        Path filePath = Paths.get("tmp-iron-test", "iron-spi-file-inttest");
+        Path filePath = get("tmp-iron-test", "iron-spi-file-inttest");
 
         String directory = "iron-directory";
         Supplier<TransactionStore> transactionStoreFactory = () -> buildFileTransactionStore(filePath, directory);
@@ -77,10 +83,57 @@ public class FileStoreTest {
             storeManager.snapshot();
         }
 
-        assertThat(Files.walk(Paths.get("tmp-iron-test")).
+        assertThat(Files.walk(get("tmp-iron-test")).
                 sorted(reverseOrder()).map(Path::toFile).
                 map(file -> new Object[]{file, file.delete()})).
                 allMatch(fileStatus -> (boolean) fileStatus[1]);
+    }
+
+    @Test
+    public void shouldSnapshotContainAllInstances() throws IOException, ExecutionException, InterruptedException {
+        // Given an iron store
+        Path filePath = get("tmp-iron-test", "iron-spi-file-inttest");
+
+        String directory = "iron-directory-" + UUID.randomUUID();
+        Supplier<TransactionStore> transactionStoreFactory = () -> buildFileTransactionStore(filePath, directory);
+        Supplier<SnapshotStore> snapshotStoreFactory = () -> buildFileSnapshotStore(filePath, directory);
+
+        TransactionSerializer transactionSerializer = buildJacksonTransactionSerializer();
+        SnapshotSerializer snapshotSerializer = buildJacksonSnapshotSerializer();
+
+        StoreManagerBuilder factoryBuilder = StoreManagerBuilder.newStoreManagerBuilder() //
+                .withTransactionSerializer(transactionSerializer) //
+                .withTransactionStore(transactionStoreFactory.get()) //
+                .withSnapshotSerializer(snapshotSerializer) //
+                .withSnapshotStore(snapshotStoreFactory.get()) //
+                .withEntityClass(Company.class) //
+                .withEntityClass(Person.class) //
+                .withCommandClass(CreatePerson.class) //
+                ;
+
+        try (StoreManager storeManager = factoryBuilder.build()) {
+            Store store1 = storeManager.getStore("store1");
+            // When I snapshot before any transaction
+            storeManager.snapshot();
+            // Then I find no snapshot
+            assertThat(Files.walk(filePath.resolve(directory).resolve("snapshot")).collect(toSet())).
+                    containsExactlyInAnyOrderElementsOf(asList(filePath.resolve(directory).resolve("snapshot")));
+            //
+            // When I snapshot after a transaction
+            Store.TransactionBuilder transaction1_1 = store1.begin();
+            transaction1_1.addCommand(CreatePerson.class).
+                    set(CreatePerson::id).to("myPersonId").
+                    set(CreatePerson::name).to("myPersonName").
+                    submit();
+            transaction1_1.submit().get();
+            storeManager.snapshot();
+            // Then I find a snapshot
+            List<Path> expectedPaths = asList(filePath.resolve(directory).resolve("snapshot"),
+                                              filePath.resolve(directory).resolve("snapshot").resolve("00000000000000000000"),
+                                              filePath.resolve(directory).resolve("snapshot").resolve("00000000000000000000").resolve("store1.snapshot"));
+            assertThat(Files.walk(filePath.resolve(directory).resolve("snapshot")).collect(toSet())).
+                    containsExactlyInAnyOrderElementsOf(expectedPaths);
+        }
     }
 }
 
