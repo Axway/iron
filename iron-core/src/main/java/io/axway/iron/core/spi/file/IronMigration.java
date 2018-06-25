a94c7a116ca291d2e7ad600cf13df3b34afc1863
gvauvert <gvauvert@axway.com>
2018-06-25 13:50:59 +0200

    59 - Fix missing snapshots

--- iron-core/src/main/java/io/axway/iron/core/spi/file/IronMigration.java
+++ iron-core/src/main/java/io/axway/iron/core/spi/file/IronMigration.java
@@ -7,8 +7,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
 import com.google.common.base.Throwables;
 import io.axway.iron.error.StoreException;
+
+import static java.util.Arrays.*;
+import static java.util.stream.Collectors.*;
 
 /**
  * Migration script to migrate iron snapshots files from 0.5.0 layout to 0.6.0 layout
@@ -24,23 +28,22 @@ public class IronMigration {
 
     public static void main(String[] args) {
         try {
+            // Check arguments
             if (args.length != 4) {
                 throw new IllegalArgumentException("Missing arguments");
             }
-
             Path ironPath = Paths.get(args[0]);
             if (!ironPath.toFile().exists() || !ironPath.toFile().isDirectory()) {
                 throw new IllegalArgumentException("Unknown iron directory : " + args[0]);
             }
-
             String globalStoreManagerName = args[1];
             String storesStoreManagerName = args[2];
             if (globalStoreManagerName.length() == 0 || storesStoreManagerName.length() == 0) {
                 throw new IllegalArgumentException("globalStoreManagerName and storesStoreManagerName must not be empty");
             }
-
             Path globalPath = ironPath.resolve(GLOBAL_DIRECTORY_NAME).resolve(SNAPSHOT_DIRECTORY_NAME);
             Path targetGlobalPath = Paths.get(args[3]).resolve(globalStoreManagerName);
+            // Copy global snapshots
             Files.walk(globalPath)                                                    //
                     .map(Path::toFile)                                                //
                     .filter(File::isFile)                                             //
@@ -55,7 +58,7 @@ public class IronMigration {
                             throw Throwables.propagate(e);
                         }
                     });
-
+            // Copy tenant snapshots
             Path targetStoresPath = Paths.get(args[3]).resolve(storesStoreManagerName);
             Files.walkFileTree(ironPath, new FileVisitor<Path>() {
                 @Override
@@ -87,7 +90,22 @@ public class IronMigration {
                     return FileVisitResult.CONTINUE;
                 }
             });
-
+            // Complete snapshot N with lacking snapshot files from snapshot N - 1
+            Set<File> previousSnapshots = new HashSet<>();
+            List<File> snapshots = asList(targetStoresPath.resolve(SNAPSHOT_DIRECTORY_NAME).toFile().listFiles());
+            Collections.sort(snapshots);
+            for (File snapshot : snapshots) {
+                Set<String> snapshotNames = Arrays.stream(snapshot.listFiles()).map(File::getName).collect(toSet());
+                previousSnapshots.stream().filter(previousSnapshot -> !snapshotNames.contains(previousSnapshot.getName())).forEach(previousSnapshot -> {
+                    try {
+                        Files.copy(previousSnapshot.toPath(), snapshot.toPath().resolve(previousSnapshot.getName()));
+                    } catch (IOException e) {
+                        throw Throwables.propagate(e);
+                    }
+                });
+                previousSnapshots.clear();
+                previousSnapshots.addAll(Arrays.stream(snapshot.listFiles()).collect(toSet()));
+            }
             System.out.println("Migration done to " + args[3]);
         } catch (Exception e) {
             System.err.println(
